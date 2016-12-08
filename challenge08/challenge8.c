f9ba6bd8452a304c1f76fc7b1043dbf673d1a54b
dobin rutishauser <dobin.rutishauser@csnc.ch>
2016-12-08 16:02:27 +0100

    added challenge8

---
+++ challenge08/challenge8.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(void) {
+        int array[5] = { 1, 2, 3, 4, 5};
+        printf("Number at index 4: 0x%x\n", array[4]);
+        printf("Number at index 5: 0x%x\n", array[5]);
+}
