9c241d7f1ab29613b7b4c28fcc0dbd80823e5058
dobinrutis <dobin@broken.ch>
2022-03-29 00:45:30 +0200

    clearer output

--- challenge02/datastructures.c
+++ challenge02/datastructures.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-void funcWithArray() {
-        unsigned int dummy1 = 0xaabbccdd;
+void Array() {
+        unsigned int stackTop = 0xaabbccdd;
         char charArray[4];
-        unsigned int dummy2 = 0x11223344;
+        unsigned int stackBot = 0x11223344;
 
-        printf("funcWithArray:\n");
+        printf("Array():\n");
 
         charArray[0] = 1;
         charArray[1] = 2;
         charArray[2] = 3;
         charArray[3] = 4;
 
-        printf("Dummy 1 (%p): %i/0x%x\n", &dummy1, dummy1, dummy1);
-        printf("Dummy 2 (%p): %i/0x%x\n\n", &dummy2, dummy2, dummy2);
-
-        printf("CharArray #0  (%p): %i/0x%hhx\n", (void *) &charArray[0], charArray[0], charArray[0]);
-        printf("CharArray #1  (%p): %i/0x%hhx\n", (void *) &charArray[1], charArray[1], charArray[1]);
-        printf("CharArray #4  (%p): %i/0x%hhx\n", (void *) &charArray[4], charArray[4], charArray[4]);
-        printf("CharArray #-1 (%p): %i/0x%hhx\n", (void *) &charArray[-1], charArray[-1], charArray[-1]);
+        printf("stackTop      @ %p: 0x%x\n", &stackTop, stackTop);
+        printf("CharArray #0  @ %p: 0x%hhx\n", (void *) &charArray[0], charArray[0]);
+        printf("CharArray #1  @ %p: 0x%hhx\n", (void *) &charArray[1], charArray[1]);
+        printf("CharArray #4  @ %p: 0x%hhx (part of stackTop)\n", (void *) &charArray[4], charArray[4]);
+        printf("CharArray #-1 @ %p: 0x%hhx (part of stackBot)\n", (void *) &charArray[-1], , charArray[-1]);
+        printf("stackBot      @ %p: 0x%x\n\n", &stackBot, stackBot);
 }
 
 
@@ -29,8 +28,8 @@ struct cStruct {
         char z[3];
 };
 
-void funcWithStruct() {
-        printf("funcWithStruct:\n");
+void Struct() {
+        printf("Struct():\n");
 
         struct cStruct cstruct;
         cstruct.x = 1;
@@ -39,38 +38,38 @@ void funcWithStruct() {
         cstruct.z[1] = 0x11;
         cstruct.z[2] = 0x12;
 
-        printf("cStruct.x    (%p): %i/0x%x)\n", (void *) &cstruct.x, cstruct.x, cstruct.x);
-        printf("cStruct.y    (%p): %ld/0x%lx)\n", (void *) &cstruct.y, cstruct.y, cstruct.y);
-        printf("cStruct.z[0] (%p): %i/0x%hhx)\n", (void *) &cstruct.z[0], cstruct.z[0], cstruct.z[0]);
-        printf("cStruct.z[1] (%p): %i/0x%hhx)\n", (void *) &cstruct.z[1], cstruct.z[1], cstruct.z[1]);
-        printf("cStruct.z[2] (%p): %i/0x%hhx)\n", (void *) &cstruct.z[1], cstruct.z[2], cstruct.z[2]);
+        printf("cStruct.x    @ %p\n", (void *) &cstruct.x);
+        printf("cStruct.y    @ %p\n", (void *) &cstruct.y);
+        printf("cStruct.z[0] @ %p\n", (void *) &cstruct.z[0]);
+        printf("cStruct.z[1] @ %p\n", (void *) &cstruct.z[1]);
+        printf("cStruct.z[2] @ %p\n", (void *) &cstruct.z[1]);
 }
 
 
-void funcWithStrcpy() {
-        printf("funcWithStrcpy:\n");
+void Strncpy() {
+        printf("Strncpy():\n");
 
-        char dest1[8] = "1111111";
-        char dest2[8] = "2222222";
+        char dest1[8] = "1234567\0";
+        char dest2[8] = "1234567\0";
 
         printf("Init:\n");
-        printf("%p Dest1 : %s\n", dest1, dest1);
-        printf("%p Dest2 : %s\n", dest2, dest2);
+        printf("dest1 @ %p: %s\n", dest1, dest1);
+        printf("dest2 @ %p: %s\n", dest2, dest2);
 
-        strncpy(dest2, "33333333", 8);
+        strncpy(dest2, "AABBCCDD", 8);
 
-        printf("After strncpy():\n");
-        printf("%p Dest1 : %s\n", dest1, dest1);
-        printf("%p Dest2 : %s\n", dest2, dest2);
+        printf("After strncpy(dest2, \"AABBCCDD\"):\n");
+        printf("dest1 @ %p: %s\n", dest1, dest1);
+        printf("dest2 @ %p: %s (missing nul terminator)\n", dest2, dest2);
 }
-
 
 void main(void) {
 
-        funcWithArray();
+        Array();
         printf("\n");
-        funcWithStruct();
-        printf("\n");
-        funcWithStrcpy();
 
+        Struct();
+        printf("\n");
+
+        Strncpy();
 }
