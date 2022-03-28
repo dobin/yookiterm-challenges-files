49142d0f4c8029c2f05b272ae6d542a1d85ee689
dobinrutis <dobin@broken.ch>
2022-03-29 01:09:41 +0200

    even better output

--- challenge02/challenge2.c
+++ challenge02/challenge2.c
@@ -1,75 +1,94 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void Array() {
         unsigned int stackTop = 0xaabbccdd;
-        char charArray[4];
+        char charArray[4] = { 1, 2, 3, 4 };
         unsigned int stackBot = 0x11223344;
 
-        printf("Array():\n");
-
-        charArray[0] = 1;
-        charArray[1] = 2;
-        charArray[2] = 3;
-        charArray[3] = 4;
-
+        printf(
+        "unsigned int stackTop = 0xaabbccdd; \n"
+        "char charArray[4] = { 1, 2, 3, 4 }; \n"
+        "unsigned int stackBot = 0x11223344; \n"
+        "\n"
+        );
+        printf("stackBot      @ %p: 0x%x\n", &stackBot, stackBot);
+        printf("charArray[-1] @ %p: 0x%hhx (part of stackBot)\n", (void *) &charArray[-1], charArray[-1]);
+        printf("charArray[0]  @ %p: 0x%hhx\n", (void *) &charArray[0], charArray[0]);
+        printf("charArray[1]  @ %p: 0x%hhx\n", (void *) &charArray[1], charArray[1]);
+        printf("charArray[4]  @ %p: 0x%hhx (part of stackTop)\n", (void *) &charArray[4], charArray[4]);
         printf("stackTop      @ %p: 0x%x\n", &stackTop, stackTop);
-        printf("CharArray #0  @ %p: 0x%hhx\n", (void *) &charArray[0], charArray[0]);
-        printf("CharArray #1  @ %p: 0x%hhx\n", (void *) &charArray[1], charArray[1]);
-        printf("CharArray #4  @ %p: 0x%hhx (part of stackTop)\n", (void *) &charArray[4], charArray[4]);
-        printf("CharArray #-1 @ %p: 0x%hhx (part of stackBot)\n", (void *) &charArray[-1], , charArray[-1]);
-        printf("stackBot      @ %p: 0x%x\n\n", &stackBot, stackBot);
 }
 
 
-struct cStruct {
+struct __attribute__((__packed__)) cStruct {
         short x;
         long y;
-        char z[3];
+        char z[2];
 };
 
 void Struct() {
-        printf("Struct():\n");
-
         struct cStruct cstruct;
         cstruct.x = 1;
         cstruct.y = 2;
         cstruct.z[0] = 0x10;
         cstruct.z[1] = 0x11;
-        cstruct.z[2] = 0x12;
 
-        printf("cStruct.x    @ %p\n", (void *) &cstruct.x);
-        printf("cStruct.y    @ %p\n", (void *) &cstruct.y);
-        printf("cStruct.z[0] @ %p\n", (void *) &cstruct.z[0]);
-        printf("cStruct.z[1] @ %p\n", (void *) &cstruct.z[1]);
-        printf("cStruct.z[2] @ %p\n", (void *) &cstruct.z[1]);
+        printf(
+        "struct cStruct { \n"
+        "        short x; \n"
+        "        long y; \n"
+        "        char z[2]; \n"
+        "}; \n"
+
+        "cstruct.x = 1; \n"
+        "cstruct.y = 2; \n"
+        "cstruct.z[0] = 0x10; \n"
+        "cstruct.z[1] = 0x11; \n"
+        "\n"
+        );
+
+        printf("cStruct.x    @ %p (size %i)\n", (void *) &cstruct.x, sizeof(cstruct.x));
+        printf("cStruct.y    @ %p (size %i)\n", (void *) &cstruct.y, sizeof(cstruct.y));
+        printf("cStruct.z[0] @ %p (size %i)\n", (void *) &cstruct.z[0], sizeof(cstruct.z[0]));
+        printf("cStruct.z[1] @ %p (size %i)\n", (void *) &cstruct.z[1], sizeof(cstruct.z[1]));
 }
 
 
 void Strncpy() {
-        printf("Strncpy():\n");
-
         char dest1[8] = "1234567\0";
         char dest2[8] = "1234567\0";
 
-        printf("Init:\n");
+        printf(
+        "char dest1[8] = \"1234567\\0\"; \n"
+        "char dest2[8] = \"1234567\\0\"; \n"
+        "\n"
+        );
+
         printf("dest1 @ %p: %s\n", dest1, dest1);
         printf("dest2 @ %p: %s\n", dest2, dest2);
 
         strncpy(dest2, "AABBCCDD", 8);
 
-        printf("After strncpy(dest2, \"AABBCCDD\"):\n");
+        printf("After strncpy(dest2, \"AABBCCDD\"), without space for nul:\n");
         printf("dest1 @ %p: %s\n", dest1, dest1);
         printf("dest2 @ %p: %s (missing nul terminator)\n", dest2, dest2);
 }
 
-void main(void) {
-
-        Array();
+void main(int argc, char **argv) {
+        if (argc != 2) {
+                printf("%s: array|struct|strncpy", argv[0]);
+                exit(1);
+        }
+        if (strcmp(argv[1], "array") == 0) {
+                Array();
+        }
+        if (strcmp(argv[1], "struct") == 0) {
+                Struct();
+        }
+        if (strcmp(argv[1], "strncpy") == 0) {
+                Strncpy();
+        }
         printf("\n");
-
-        Struct();
-        printf("\n");
-
-        Strncpy();
 }
