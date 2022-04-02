834890cd9e989dc99e433795936e0e47c0310439
dobinrutis <dobin@broken.ch>
2022-04-02 13:38:00 +0200

    rework: batch 2

--- challenge07/challenge07.c
+++ challenge07/challenge07.c
@@ -6,27 +6,20 @@ void functionMinimal(void) {
         return;
 }
 
-int functionBasic(int c) {
+void functionInt(int c) {
         int d;
         d = c + 1;
-        return d;
 }
 
-char* functionAdvanced(char *a, char *b) {
+void functionStrcpy(char *a, char *b) {
         strcpy(a, b);
-        return a;
 }
-
 
 int main(int argc, char **argv) {
         char *a = "AAAAA";
         char *b = "BBB";
-        int ret2;
-        char *ret3;
 
         functionMinimal();
-
-        ret2 = functionBasic(5);
-
-        ret3 = functionAdvanced(a, b);
+        functionInt(5);
+        functionStrcpy(a, b);
 }
