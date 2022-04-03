848b8b56aab77b2f292ac39dfaca118879eb20bc
dobinrutis <dobin@broken.ch>
2022-04-03 14:28:13 +0200

    fix challenge08

--- challenge08/challenge08.c
+++ challenge08/challenge08.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main(int argc, char **argv) {
         if (argc != 2) {
-                printf("Usage: %s <idx>", argv[0])
-                return
+                printf("Usage: %s <idx>\n", argv[0]);
+                return;
         }
 
-        int array[5] = { 0, 1, 2, 3, 4 };
+        int array[4] = { 0xAA, 0xBB, 0xCC, 0xDD };
         int idx = atoi(argv[1]);
         printf("Number at index %i: 0x%x\n", idx, array[idx]);
 }
