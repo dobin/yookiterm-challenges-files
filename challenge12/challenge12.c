75c89cabbdb17d149ae6aca7a72a553be8ff2e3b
Dobin <dobin.rutishauser@gmail.com>
2022-04-09 19:42:54 +0000

    update challenge12

--- challenge12/challenge12.c
+++ challenge12/challenge12.c
@@ -3,9 +3,8 @@
 #include <crypt.h>
 #include <string.h>
 
-const char *adminHash = "$6$saaaaalty$cjw9qyAKmchl7kQMJxE5c1mHN0cXxfQNjs4EhcyULLndQR1wXslGCaZrJj5xRRBeflfvmpoIVv6Vs7ZOQwhcx.";
-
 int checkPassword(char *password) {
+    char *adminHash = "$6$saaaaalty$cjw9qyAKmchl7kQMJxE5c1mHN0cXxfQNjs4EhcyULLndQR1wXslGCaZrJj5xRRBeflfvmpoIVv6Vs7ZOQwhcx.";
     char *hash = crypt(password, "$6$saaaaalty");
     if (strcmp(hash, adminHash) == 0) {
         return 1;
@@ -14,25 +13,41 @@ int checkPassword(char *password) {
     }
 }
 
+void secret() {
+    printf("Secret functionality\n");
+}
+
 void handleData(char *username, char *password) {
     int isAdmin = 0;
-    char name[64];
-    memset(name, 'X', 64);
-    
+    char name[128];
+
     isAdmin = checkPassword(password);
     strcpy(name, username);
 
+    printf("isAdmin: 0x%x\n", isAdmin);
     if(isAdmin > 0) {
-        printf("You are admin!\nisAdmin: 0x%x\n", isAdmin);
+        printf("You are admin!\n");
     } else {
-        printf("You are not admin.\nisAdmin: 0x%x\n", isAdmin);
+        printf("Not admin.\n");
     }
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Arguments: %s <name> <password>\n", argv[0]);
+    char username[1024];
+    char password[32];
+    bzero(username, sizeof(username));
+    bzero(password, sizeof(password));
+
+    if (argc != 1) {
+        printf("Usage: %s\n", argv[0]);
         exit(0);
     }
-    handleData(argv[1], argv[2]);
+
+    printf("Username: "); fflush(stdout);
+    read(0, username, sizeof(username));
+
+    printf("Password: "); fflush(stdout);
+    read(0, password, sizeof(password));
+
+    handleData(username, password);
 }
